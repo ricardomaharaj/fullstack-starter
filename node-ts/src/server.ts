667c474ec9601f0341eeaed957653b8c78908baa
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-03 23:45:33 -0400

    update template

--- node-ts/src/server.ts
+++ node-ts/src/server.ts
@@ -1,1 +1,7 @@
-console.log('!')
+import { createServer } from 'node:http'
+
+const server = createServer((req, res) => {
+  res.end(`hello from node @ ${new Date().toString()}`)
+})
+
+server.listen(4000, () => console.log('http://localhost:4000/'))

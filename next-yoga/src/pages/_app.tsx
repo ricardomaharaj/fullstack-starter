b8a66db0e2077e67d2c93ad89ff2051439de37f9
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-08 22:46:21 -0400

    add urql

--- next-yoga/src/pages/_app.tsx
+++ next-yoga/src/pages/_app.tsx
@@ -1,5 +1,20 @@
 import type { AppProps } from 'next/app'
+import {
+  createClient as createUrql,
+  Provider as UrqlProvider,
+  cacheExchange,
+  fetchExchange,
+} from 'urql'
+
+const urql = createUrql({
+  url: '/api/gql',
+  exchanges: [cacheExchange, fetchExchange],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UrqlProvider value={urql}>
+      <Component {...pageProps} />
+    </UrqlProvider>
+  )
 }

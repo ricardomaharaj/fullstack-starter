156ca689a9e990340115604c763af4d974350412
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-12 21:01:12 -0400

    remove old starters

--- qwik/src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-import { component$ } from '@builder.io/qwik'
-
-const Home = component$(() => {
-  return <></>
-})
-
-export default Home
--- t3/src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next'
-import { createTRPCContext } from '@/server/api/trpc'
-import { appRouter } from '@/server/api/root'
-
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-})
